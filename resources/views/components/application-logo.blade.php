78981a90cae0aa620ec6bbf0e8f136752e92492d
Alexander <alexander.ahman@student.hv.se>
2026-08-26 06:55:34 +0200

    feat: restyle shared Breeze components onto the tn-* design system

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,8 +1,8 @@
 <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
     <defs>
         <linearGradient id="wpGrad" x1="0" y1="0" x2="1" y2="1">
-            <stop offset="0%" stop-color="#06b6d4" />
-            <stop offset="100%" stop-color="#22c55e" />
+            <stop offset="0%" stop-color="#8288ff" />
+            <stop offset="100%" stop-color="#4f6bff" />
         </linearGradient>
     </defs>
     <rect x="2" y="2" width="60" height="60" rx="16" fill="#0f172a" stroke="url(#wpGrad)" stroke-width="2" />
