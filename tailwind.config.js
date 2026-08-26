7f8834feeb0185908f476099e7daed35d8de60cf
Alexander <alexander.ahman@student.hv.se>
2026-08-26 06:52:33 +0200

    feat: add TeamNest brand color scale and tn-* design system classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,21 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Manrope', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                brand: {
+                    50: '#eef1ff',
+                    100: '#e0e4ff',
+                    200: '#c6ccff',
+                    300: '#a5aeff',
+                    400: '#8288ff',
+                    500: '#4f6bff',
+                    600: '#3d52e0',
+                    700: '#3040b8',
+                    800: '#293593',
+                    900: '#252f75',
+                },
             },
         },
     },
